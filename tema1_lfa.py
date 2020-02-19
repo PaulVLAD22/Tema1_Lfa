1235dfb65276f9684effd52d7be305cc20ca2184
Vlad Munteanu <paulvlad34@gmail.com>
2020-02-19 23:41:57 +0200

    Update tema1_lfa.py

--- tema1_lfa.py
+++ tema1_lfa.py
@@ -16,41 +16,44 @@ while (sir):
     sir=f.readline()
 print(adiacenta)
 sir=input("Introduceti cuvantul")
-ex_litere=0
-ex_cifre=0
-drum=[]
-for x in sir:
-    drum.append(x)
-    if(x.isdecimal()):
-        ex_cifre=1
-    elif (x in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ"):
-        ex_litere=1
-    else:
-        ex_cifre=2
-print(st_initial)
-print(st_final)
-print(traducere)
-if (ex_litere+ex_cifre==1):
-    print(st_initial)
-    poz=traducere.index(st_initial)
-    st_final_numere=[]
-    for x in st_final:
-        st_final_numere.append(traducere.index(x))
-    print(st_final_numere)
-    log=0
-    for x in drum:
-        for k in range(n):
-            if (x in adiacenta[poz][k]):
-                poz=k
-                print(poz)
-                break
+if (sir!="λ"):
+    ex_litere=0
+    ex_cifre=0
+    drum=[]
+    for x in sir:
+        drum.append(x)
+        if(x.isdecimal()):
+            ex_cifre=1
+        elif (x in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ"):
+            ex_litere=1
         else:
+            ex_cifre=2
+    print(st_initial)
+    print(st_final)
+    print(traducere)
+    if (ex_litere+ex_cifre==1):
+        print(st_initial)
+        poz=traducere.index(st_initial)
+        st_final_numere=[]
+        for x in st_final:
+            st_final_numere.append(traducere.index(x))
+        print(st_final_numere)
+        log=0
+        for x in drum:
+            for k in range(n):
+                if (x in adiacenta[poz][k]):
+                    poz=k
+                    print(poz)
+                    break
+            else:
+                log=1
+        if(poz not in st_final_numere):
             log=1
-    if(poz not in st_final_numere):
-        log=1
-    if (log==0):
-        print("Da")
+        if (log==0):
+            print("Da")
+        else:
+            print("Drumul este gresit")
     else:
         print("Drumul este gresit")
 else:
-    print("Drumul este gresit")
+    print("Da")
